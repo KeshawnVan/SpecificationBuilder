5bb26ed653887ce4d9497ea6cbb6fc25d2300046
Fankx <fankx@startimes.com.cn>
2018-07-16 14:17:56 +0800

    implement join

---
+++ src/main/java/demo/ReflectionUtil.java
@@ -0,0 +1,120 @@
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+/**
+ * @author keshawn
+ * @date 2017/11/9
+ */
+public final class ReflectionUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    private ReflectionUtil() {
+    }
+
+    public static <T> T newInstance(Class<T> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return (T) instance;
+    }
+
+    public static Object invokeMethod(Object object, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(object, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static void setField(Object object, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (Exception e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Object getField(Field field, Object object) {
+        field.setAccessible(true);
+        try {
+            return field.get(object);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("get field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Field> getFields(Class clazz) {
+        List<Field> result = new ArrayList<>(Arrays.asList(clazz.getDeclaredFields()));
+        Optional.ofNullable(clazz.getSuperclass()).ifPresent(addSuperclassFields(result));
+        return result;
+    }
+
+    private static Consumer<Class> addSuperclassFields(List<Field> result) {
+        return superClass -> {
+            List<Field> superclassFields = getFields(superClass);
+            List<String> resultNames = result.stream().map(Field::getName).collect(Collectors.toList());
+            List<Field> validSuperclassFields = superclassFields.stream().filter(superclassField -> !resultNames.contains(superclassField.getName())).collect(Collectors.toList());
+            result.addAll(validSuperclassFields);
+        };
+    }
+
+    public static Type[] getActualTypeArguments(Type type) {
+        return ((ParameterizedType) type).getActualTypeArguments();
+    }
+
+    /**
+     * ParameterizedType类型的Type获取外层类型
+     * 如：传入List<User>，返回List
+     *
+     * @param type
+     * @return
+     */
+    public static Class<?> getRawClass(Type type) {
+        return ((ParameterizedTypeImpl) type).getRawType();
+    }
+
+
+    /**
+     * 检查是否是基本类型或其包装类型
+     *
+     * @param type
+     * @return
+     */
+    public static boolean isPrimitive(Class<?> type) {
+        return type.isPrimitive()
+                || type.equals(Integer.class)
+                || type.equals(String.class)
+                || type.equals(Long.class)
+                || type.equals(Double.class)
+                || type.equals(Byte.class)
+                || type.equals(Short.class)
+                || type.equals(Float.class)
+                || type.equals(Character.class);
+    }
+}
--- src/main/java/demo/SpecificationBuilder.java
+++ src/main/java/demo/SpecificationBuilder.java
@@ -1,7 +1,6 @@
 package demo;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.Specification;
@@ -14,6 +13,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author keshawn
@@ -25,39 +25,36 @@ public final class SpecificationBuilder {
 
     private static final String PERCENT = "%";
 
+    private static final String SERIAL_VERSION_UID = "serialVersionUID";
+
     private SpecificationBuilder() {
     }
 
     public static <T> Specification buildSpecification(T condition) {
         Class<?> conditionClass = condition.getClass();
-        Field[] declaredFields = conditionClass.getDeclaredFields();
-        Specification specification = (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-            if (ArrayUtils.isNotEmpty(declaredFields)) {
-                fieldParse(root, predicates, cb, condition, declaredFields);
-            }
+        List<Field> declaredFields = ReflectionUtil.getFields(conditionClass);
+        return (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>(declaredFields.size());
+            Nullable.of(declaredFields).ifPresent(fields -> fields.stream().filter(checkFiledAccess()).forEach(fieldParse(root, predicates, cb, condition)));
             return cb.and(predicates.toArray(new Predicate[predicates.size()]));
         };
-        return specification;
     }
 
-    private static <T> void fieldParse(Root root, List<Predicate> predicates, CriteriaBuilder cb, T condition, Field[] declaredFields) {
-        for (Field field : declaredFields) {
+    private static java.util.function.Predicate<Field> checkFiledAccess() {
+        return field -> !(field.isAnnotationPresent(Page.class) || field.isAnnotationPresent(Ignore.class) || SERIAL_VERSION_UID.equals(field.getName()));
+    }
+
+    private static <T> Consumer<Field> fieldParse(Root root, List<Predicate> predicates, CriteriaBuilder cb, T condition) {
+        return field -> {
             try {
-                if (field.isAnnotationPresent(Page.class) || field.isAnnotationPresent(Ignore.class)) {
-                    continue;
-                }
                 field.setAccessible(true);
                 //优先从Name注解上取自定义名称
                 String fieldName = field.isAnnotationPresent(Name.class) ? field.getAnnotation(Name.class).value() : field.getName();
-                Object fieldObject = field.get(condition);
-                if (fieldObject != null) {
-                    appendPredicate(root, predicates, field, fieldName, fieldObject, cb);
-                }
+                Nullable.of(field.get(condition)).ifPresent(fieldObject -> appendPredicate(root, predicates, field, fieldName, fieldObject, cb));
             } catch (IllegalAccessException e) {
                 LOGGER.error("SpecificationBuilder buildSpecification fieldParse error", e);
             }
-        }
+        };
     }
 
     private static void appendPredicate(Root root, List<Predicate> predicates, Field field, String fieldName, Object fieldObject, CriteriaBuilder cb) {
@@ -65,7 +62,12 @@ public final class SpecificationBuilder {
         if (Collection.class.isAssignableFrom(field.getType())) {
             Collection collectionObject = (Collection) fieldObject;
             if (CollectionUtils.isNotEmpty(collectionObject)) {
-                predicates.add(root.get(fieldName).in(collectionObject));
+                if (field.isAnnotationPresent(Join.class)) {
+                    Join join = field.getAnnotation(Join.class);
+                    predicates.add(root.get(join.value()).get(fieldName).in(collectionObject));
+                } else {
+                    predicates.add(root.get(fieldName).in(collectionObject));
+                }
             }
         } else {
             if (field.isAnnotationPresent(Like.class)) {
@@ -79,7 +81,11 @@ public final class SpecificationBuilder {
             } else if (field.isAnnotationPresent(LessThanEqual.class)) {
                 predicates.add(cb.lessThanOrEqualTo(root.get(fieldName), (Comparable) fieldObject));
             } else {
-                predicates.add(cb.equal(root.get(fieldName), fieldObject));
+                if (field.isAnnotationPresent(Join.class)) {
+                    predicates.add(cb.equal(root.get(field.getAnnotation(Join.class).value()).get(fieldName), fieldObject));
+                } else {
+                    predicates.add(cb.equal(root.get(fieldName), fieldObject));
+                }
             }
         }
     }
@@ -87,14 +93,15 @@ public final class SpecificationBuilder {
     private static void likeHandler(Root root, List<Predicate> predicates, Field field, String fieldName, Object fieldObject, CriteriaBuilder cb) {
         Like like = field.getAnnotation(Like.class);
         String location = like.location();
-        if (Like.AROUND.equals(location)){
+        if (Like.AROUND.equals(location)) {
             predicates.add(cb.like(root.get(fieldName), PERCENT + fieldObject + PERCENT));
         }
-        if (Like.LEFT.equals(location)){
+        if (Like.LEFT.equals(location)) {
             predicates.add(cb.like(root.get(fieldName), PERCENT + fieldObject));
         }
-        if (Like.RIGHT.equals(location)){
+        if (Like.RIGHT.equals(location)) {
             predicates.add(cb.like(root.get(fieldName), fieldObject + PERCENT));
         }
     }
 }
+
